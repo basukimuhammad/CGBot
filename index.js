d0c75df959c8fcb05cf86d0bfc3b92d7d18665dd
basukimuhammad <116169432+basukimuhammad@users.noreply.github.com>
2024-07-12 14:26:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.get("/",  (req, res) => {
-    res.sendFile(__dirname + "index.html")
+// Layani file statis dari direktori "public"
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rute untuk melayani file HTML
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
-    console.log(`Server berjalan di http://localhost:${port}`);
+  console.log(`Server berjalan di http://localhost:${port}`);
 });
